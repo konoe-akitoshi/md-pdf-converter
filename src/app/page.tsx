3cb37dc23b57735e8e391b169d7a9a328b34702b
Akitoshi Saeki <51017077+konoe-akitoshi@users.noreply.github.com>
2025-05-20 13:50:29 +0900

    スタイル: GitHub Markdown CSSを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
+import "github-markdown-css/github-markdown.css";
 
 export default function Home() {
   const [markdown, setMarkdown] = useState<string>(
